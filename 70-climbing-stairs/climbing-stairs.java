1793bcbe7b8727080fed7a05c3a24d18228c87c4
Fazliya1610 <119477899+Fazliya1610@users.noreply.github.com>
2024-01-19 17:40:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (31.96%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) {
+            return n;
+        }
+        int[] step = new int[n+1];
+        step[1] = 1;
+        step[2] = 2;
+        for(int i=3;i<=n;i++) {
+            step[i] = step[i-1]+step[i-2];
+        }
+        return step[n];
+    }
+}
