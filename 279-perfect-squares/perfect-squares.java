dacedfea1f6aec2872dcc8d2fdc933e8563837c9
Fazliya1610 <119477899+Fazliya1610@users.noreply.github.com>
2024-02-08 08:56:14 +0530

    Time: 35 ms (62.09%) | Memory: 42.5 MB (79.98%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] res = new int[n+1];
+        for(int i=1;i<=n;i++) {
+            res[i] = Integer.MAX_VALUE;
+        }
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j*j<=i;j++) {
+                res[i] = Math.min(res[i], res[i-j*j]+1);
+            }
+        }
+        return res[n];
+    }
+}
