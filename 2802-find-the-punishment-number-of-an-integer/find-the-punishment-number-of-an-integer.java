a55cc84c3b666e2c8a39f433a38929bde167cf17
Fazliya1610 <119477899+Fazliya1610@users.noreply.github.com>
2025-02-15 14:18:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (99.37%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    int[] arr = {1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000};
+    public int punishmentNumber(int n) {
+        int res = 0;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]<=n) {
+                res+=arr[i]*arr[i];
+            }
+            else {
+                break;
+            }
+        }
+        return res;
+    }
+}
